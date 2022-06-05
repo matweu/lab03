1e5a03c643a1d1fe14dbacc38ef970ca544919a2
Matwey <zhukall@mail.ru>
2022-06-06 01:44:07 +0300

    Выполнено 3.2.9

--- main.cpp
+++ main.cpp
@@ -57,7 +57,7 @@ read_input(istream& in,bool prompt) {
 
     if(prompt)
     cerr << "Enter numbers: ";
-    data.numbers = input_numbers(cin,number_count);
+    data.numbers = input_numbers(in,number_count);
 
     if(prompt)
     cerr <<"Enter bin count:";
@@ -109,29 +109,52 @@ show_histogram_text(const vector<double>bins)
         cout << endl;
     }
 }
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+    size_t data_size = item_size * item_count;
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(reinterpret_cast<char*>(items), data_size);
+    return data_size;
+}
+
+Input
+download(const string& address)
+{
+    stringstream buffer;
+    CURL *curl = curl_easy_init();
+    if(curl)
+    {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK){
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+                    exit(1);
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    return read_input(buffer, false);
+}
+
 
 
 int main(int argc, char* argv[])
 {
-    if(argc>1)
-    {
-        CURL *curl = curl_easy_init();
-        if(curl)
-        {
-            CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-            res = curl_easy_perform(curl);
-            if(res != CURLE_OK)
-                fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                        curl_easy_strerror(res));
-
-            curl_easy_cleanup(curl);
-        }
-        return 0;
-    }
     curl_global_init(CURL_GLOBAL_ALL);
     Input input;
-    input = read_input(cin,true);
+    if (argc > 1)
+    {
+        input = download(argv[1]);
+    }
+    else
+    {
+        input = read_input(cin, true);
+    }
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
 
